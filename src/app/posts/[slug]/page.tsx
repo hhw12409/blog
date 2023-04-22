123e7f89727e2ca50b880105254be434947004b7
hhw12409 <sksk2425@naver.com>
2023-04-22 18:09:24 +0900

    feat: make PostContent component

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,7 @@
-import MarkdownViewer from "@/components/MarkdownViewer";
+import PostContent from "@/components/PostContent";
 import { getPostData } from "@/core/posts";
 import Image from "next/image";
 import React from "react";
-import { AiTwotoneCalendar } from "react-icons/ai";
 
 type Props = {
   params: {
@@ -12,26 +11,18 @@ type Props = {
 
 export default async function PostPage({ params: { slug } }: Props) {
   const post = await getPostData(slug);
+  const { path, title } = post;
 
   return (
     <article className="m-4 overflow-hidden bg-gray-100 shadow-lg rounded-2xl">
       <Image
         className="w-full h-1/5 max-h-[500px]"
-        src={`/images/posts/${post.path}.png`}
-        alt={post.title}
+        src={`/images/posts/${path}.png`}
+        alt={title}
         width={760}
         height={420}
       />
-      <section className="flex flex-col p-4">
-        <div className="flex items-center self-end text-sky-600">
-          <AiTwotoneCalendar />
-          <p className="ml-2 font-semibold">{post.date.toString()}</p>
-        </div>
-        <h1 className="text-4xl font-bold">{post.title}</h1>
-        <p className="text-xl font-bold">{post.description}</p>
-        <div className="mt-4 mb-8 border-2 w-44 border-sky-600" />
-        <MarkdownViewer content={post.content} />
-      </section>
+      <PostContent post={post} />
     </article>
   );
 }
---
+++ src/components/PostContent.tsx
@@ -0,0 +1,23 @@
+import MarkdownViewer from "@/components/MarkdownViewer";
+import { IPostData } from "@/types/post";
+import { AiTwotoneCalendar } from "react-icons/ai";
+
+type Props = {
+  post: IPostData;
+};
+
+export default function PostContent({ post }: Props) {
+  const { date, title, description, content } = post;
+  return (
+    <section className="flex flex-col p-4">
+      <div className="flex items-center self-end text-sky-600">
+        <AiTwotoneCalendar />
+        <p className="ml-2 font-semibold">{date.toString()}</p>
+      </div>
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <p className="text-xl font-bold">{description}</p>
+      <div className="mt-4 mb-8 border-2 w-44 border-sky-600" />
+      <MarkdownViewer content={content} />
+    </section>
+  );
+}
