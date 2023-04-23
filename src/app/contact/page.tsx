a92acf30d7eaa14211c1890b8b870c55c2ab460f
hhw12409 <sksk2425@naver.com>
2023-04-24 00:03:29 +0900

    feat: contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,38 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+
 export default function ContactPage() {
-  return <main className="bg-gray-500">Contact</main>;
+  const LINKS = [
+    {
+      icon: <AiFillGithub />,
+      url: "https://github.com/hhw12409",
+    },
+    {
+      icon: <AiFillLinkedin />,
+      url: "https://www.linkedin.com/in/%ED%98%84%EC%9A%B0-%ED%99%A9-08445523a/",
+    },
+  ];
+
+  const TITLE_CLASS = "font-bold text-3xl";
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className={`${TITLE_CLASS} my-2`}>Contact Me</h2>
+      <p>sksk2425@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-yellow-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className={`${TITLE_CLASS} my-8`}>Or Send me an Email</h2>
+      <ContactForm />
+    </section>
+  );
 }
