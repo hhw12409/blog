dc51f65dd39cc9ac17e2f463ac92f5a7fe033d3e
hhw12409 <sksk2425@naver.com>
2023-05-28 01:25:13 +0900

    style: delete header search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,6 @@ import { useRecoilState } from "recoil";
 
 export default function Header() {
   const LINK_CLASS = "transition-all hover:text-xl";
-  const [inputValue, setInputValue] = useRecoilState(searchState);
-
-  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.currentTarget.value);
-  };
 
   return (
     <header className="flex items-center justify-between p-4">
@@ -18,15 +13,6 @@ export default function Header() {
         <h1 className="text-3xl font-bold">Hyeonwoo-Blog</h1>
       </Link>
       <nav className="flex gap-4">
-        <div className="flex items-center">
-          <AiOutlineSearch size={22} className="mr-2" />
-          <input
-            placeholder="검색하기"
-            value={inputValue}
-            onChange={handleInputValue}
-          />
-        </div>
-
         {["home", "about", "posts", "contact"].map((link) => (
           <Link
             key={link}
--- src/components/SearchedPosts.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { getAllPosts } from "@/core/posts";
-
-export default async function SearchedPosts() {
-  const posts = await getAllPosts();
-  return <div>SearchedPosts</div>;
-}
